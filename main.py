3074b0afeeab3f8755e61c0c2f8ceb9b8930fc6f
szymon <Szymon.Skrzypczyk@vp.pl>
2023-03-17 12:39:59 +0100

    grid height system has to be improved

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from queue import Queue
-from threading import Thread
-from typing import Union, Tuple
-from pathlib import Path
-from PIL import Image
+from typing import Tuple
 import typer
+from _compose import compose
 
 N = 4
 SIZE = 400, 400
 app = typer.Typer()
+
+
+@app.command()
+def create(directory: str, size: Tuple[int, int], recursive: bool = False, target: str = None):
+    image = compose(directory, size, recursive)
+    if target is not None:
+        image.save(str(target))
+    else:
+        image.show()
+
 
 if __name__ == '__main__':
     app()
